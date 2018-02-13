f291997f9edddaba886223b4ed7155b91ce14591
salan <salanhess@126.com>
2018-02-14 01:44:40 +0800

    add white list

--- src/guitools/cleanup.gtpl
+++ src/guitools/cleanup.gtpl
@@ -3,12 +3,9 @@
 <title></title>
 </head>
 <body>
-<table>
-<tr>
-<td>
 <form action="/cleanup" method="post">
-	账号名:<input type="text" name="username">
-	密码:<input type="password" name="password">
+	<!-- 账号名:<input type="text" name="username">
+	密码:<input type="password" name="password"> -->
 	邮箱:<input type="text" name="email">
 	tenant_id:<input type="text" name="tenant_id">
     操作：<select name="operation">
@@ -17,9 +14,8 @@
     <option value="delsnapshot">删除快照(试用版)</option>
     </select>
 	操作原因:<input type="text" name="note">
+	<input type="hidden" name="token" value="{{.}}">
 	<input type="submit" value="提交">
 </form>
-</td>
-</tr>
 </body>
 </html>
--- src/guitools/main.go
+++ src/guitools/main.go
@@ -2,17 +2,21 @@ package main
 
 //refer to https://github.com/astaxie/build-web-application-with-golang/blob/master/zh/03.2.md
 //refer to http://wiki.jikexueyuan.com/project/the-way-to-go/15.3.html
+//Error handler refer to http://rgyq.blog.163.com/blog/static/3161253820139303344753/
 //env in
 // [root@hc172-80 delvol]# pwd
 // /export/baihao/jcloud-zbs/src/jd.com/zbs/zbs-test/tools/delvol
 // write file: https://gocn.io/article/40
+//todo: add email and tenant_id white list via json?
 
 import (
 	"bytes"
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -23,10 +27,72 @@ import (
 	"time"
 )
 
+type Tenant struct {
+	Tenant_id string
+}
+
+type Mail struct {
+	Email string
+}
+
+type Tidslice struct {
+	Tenants []Tenant
+	Emails  []Mail
+}
+
+func validateroles(cfg string, checkField string, target string) bool {
+	var s Tidslice
+	var pwd, _ = os.Getwd()
+	var CfgPath string = pwd + string(os.PathSeparator) + cfg
+	fmt.Println(CfgPath)
+	fmt.Println("target string is: ", target)
+	raw, err := ioutil.ReadFile(CfgPath)
+	check(err)
+	err = json.Unmarshal(raw, &s)
+	check(err)
+	switch {
+	case checkField == "email":
+		for i, v := range s.Emails {
+			fmt.Println(checkField+"[", i, "] =", v)
+			if target == v.Email {
+				return true
+			}
+		}
+		return false
+	case checkField == "tid":
+		for i, v := range s.Tenants {
+			fmt.Println(checkField+"[", i, "] =", v)
+			if target == v.Tenant_id {
+				return true
+			}
+		}
+		return false
+	default:
+		fmt.Println("not check")
+		return false
+	}
+
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+//out put err in ser console and web
+func errhandle(w http.ResponseWriter, out string) {
+	fmt.Println(out)
+	fmt.Fprintf(w, out)
+	time.Sleep(10 * time.Microsecond)
+}
+
+func wfile(file *os.File, str string) {
+	//写文件
+	byteSlice := []byte(str + " ")
+	bytesWritten, err := file.Write(byteSlice)
+	check(err)
+	fmt.Println("Wrote %d bytes.\n", bytesWritten)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -55,55 +121,59 @@ func cleanup(w http.ResponseWriter, r *http.Request) {
 		file, err := os.OpenFile("log/operation.log", os.O_APPEND|os.O_WRONLY, 0666)
 		check(err)
 		defer file.Close()
-		//写文件
-		byteSlice := []byte(time.Now().String() + " Start to write Bytes log!\n")
-		bytesWritten, err := file.Write(byteSlice)
-		check(err)
-		fmt.Println("Wrote %d bytes.\n", bytesWritten)
+		wfile(file, time.Now().String()+" Start to write Bytes log!\n")
 		//请求的是登录数据，那么执行登录的逻辑判断
 		r.ParseForm()
 		token := r.Form.Get("token")
 		if token != "" {
 			//验证token的合法性
 			fmt.Println("Verify token wether correct...")
+			wfile(file, "token: "+token+" ")
 		} else {
 			//不存在token报错
 			fmt.Println("Error,token not exist")
+			http.Error(w, err.Error(), 500)
 		}
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-		//写文件
-		bytesWritten, err = file.Write([]byte(strings.Join(r.Form["username"], " ") + " "))
-		check(err)
-		fmt.Println("Wrote %d bytes.\n", bytesWritten)
-		//写文件
-		bytesWritten, err = file.Write([]byte(strings.Join(r.Form["password"], " ") + " "))
-		check(err)
-		fmt.Println("Wrote %d bytes.\n", bytesWritten)
-		if len(r.Form["email"][0]) == 0 {
-			//为空的处理
+		//		fmt.Println("username:", r.Form["username"])
+		//		fmt.Println("password:", r.Form["password"])
+		//		wfile(file, strings.Join(r.Form["username"], " "))
+		//		wfile(file, strings.Join(r.Form["password"], " "))
+		if r.Form["email"][0] == "" {
 			fmt.Println("email add must input!")
 		} else {
 			if m, _ := regexp.MatchString(`^([\w\.\_]{2,10})@jd.com$`, r.Form.Get("email")); !m {
 				//if m, _ := regexp.MatchString(`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`, r.Form.Get("email")); !m {
-				fmt.Println("email addr format not corret!")
+				errhandle(w, "email addr format not corret!")
+				return
 			} else {
-				fmt.Println("email:", r.Form["email"])
-				//写文件
-				bytesWritten, err = file.Write([]byte(strings.Join(r.Form["email"], " ") + " "))
-				check(err)
-				fmt.Println("Wrote %d bytes.\n", bytesWritten)
+				if validateroles("cfg.json", "email", r.Form["email"][0]) {
+					fmt.Println("email:", r.Form["email"])
+					wfile(file, strings.Join(r.Form["email"], " "))
+				} else {
+					errhandle(w, "email addr not in white list!")
+					return
+				}
 			}
 		}
-		if len(r.Form["tenant_id"][0]) == 0 {
-			//为空的处理
-			fmt.Println("tenant_id  must input!")
+
+		if r.Form["tenant_id"][0] == "" {
+			errhandle(w, "tenant_id  must input!")
+			return
 		} else {
-			fmt.Println("tenant_id:", r.Form["tenant_id"])
-			//写文件
-			bytesWritten, err = file.Write([]byte(strings.Join(r.Form["tenant_id"], "") + " "))
-			check(err)
-			fmt.Println("Wrote %d bytes.\n", bytesWritten)
+			if validateroles("cfg.json", "tid", r.Form["tenant_id"][0]) {
+				fmt.Println("tenant_id:", r.Form["tenant_id"])
+				wfile(file, strings.Join(r.Form["tenant_id"], " "))
+			} else {
+				errhandle(w, "tenant_id not in white list!")
+				return
+			}
+		}
+		if r.Form["note"][0] == "" {
+			errhandle(w, "Operation reason  must input!")
+			return
+		} else {
+			fmt.Println("note:", r.Form["note"])
+			wfile(file, strings.Join(r.Form["note"], ""))
 		}
 		fmt.Println("ZBS Operation:", r.Form["operation"])
 		if r.Form["operation"][0] == "delvolume" {
@@ -112,26 +182,13 @@ func cleanup(w http.ResponseWriter, r *http.Request) {
 			if err != nil {
 				fmt.Fprintf(w, "Error parameter not correct!") //这个写入到w的是输出到客户端的
 			}
-			fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
-			fmt.Fprintf(w, out)              //这个写入到w的是输出到客户端的
+			wfile(file, "ZBS Operation:"+"sh del_vol.sh "+r.Form["tenant_id"][0])
+			fmt.Fprintf(w, "ZBS Operation output: "+out) //这个写入到w的是输出到客户端的
 		}
-		//写文件
-		bytesWritten, err = file.Write([]byte(strings.Join(r.Form["operation"], "") + " "))
-		check(err)
-		fmt.Println("Wrote %d bytes.\n", bytesWritten)
-		if len(r.Form["note"][0]) == 0 {
-			//为空的处理
-			fmt.Println("Operation reason  must input!")
-		} else {
-			fmt.Println("note:", r.Form["note"])
-			//写文件
-			bytesWritten, err = file.Write([]byte(strings.Join(r.Form["note"], "") + "\n"))
-			check(err)
-			fmt.Println("Wrote %d bytes.\n", bytesWritten)
-		}
-		fmt.Fprintf(w, "Cleanup finished...!") //这个写入到w的是输出到客户端的
+		wfile(file, strings.Join(r.Form["operation"], "")+"\n")
+		fmt.Fprintf(w, "Cleanup finished...!")                 //这个写入到w的是输出到客户端的
+		fmt.Fprintf(w, "Welcome to ZBS disk cleanup process!") //这个写入到w的是输出到客户端的
 	}
-	fmt.Fprintf(w, "Welcome to cleanup process!") //这个写入到w的是输出到客户端的
 }
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
