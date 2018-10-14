8421970d257d0a825be16813b166f4507358a52e
salanhess <salanhess@126.com>
2018-10-14 23:49:52 +0800

    add books

--- src/jike/D15_pointer2/pointer2.go
+++ src/jike/D15_pointer2/pointer2.go
@@ -1,5 +1,18 @@
 package main
 
+/*
+有了这个偏移量，又有了结构体值在内存中的起始存储地址（这里由dogPtr变量代
+表），把它们相加我们就可以得到dogP的name字段值的起始存储地址了。这个地址由变
+量namePtr代表。
+此后，我们可以再通过两次类型转换把namePtr的值转换成一个*string类型的值，这样
+就得到了指向dogP的name字段值的指针值。
+你可能会问，我直接用取址表达式&(dogP.name)不就能拿到这个指针值了吗？干嘛绕这
+么大一圈呢？你可以想象一下，如果我们根本就不知道这个结构体类型是什么，也拿不到
+dogP这个变量，那么还能去访问它的name字段吗？
+答案是，只要有namePtr就可以。它就是一个无符号整数，但同时也是一个指向了程序内
+部数据的内存地址。它可能会给我们带来一些好处，比如可以直接修改埋藏得很深的内部
+数据
+*/
 import (
 	"fmt"
 	"unsafe"
@@ -24,6 +37,7 @@ func main() {
 	namePtr := dogPtr + unsafe.Offsetof(dogP.name)
 	nameP := (*string)(unsafe.Pointer(namePtr))
 	fmt.Printf("namePtr is %v\n", namePtr)
+	fmt.Printf("[nameP] is %v\n", nameP)
 	newDog := "Huge Ptr dog"
 	fmt.Printf("newDog Ptr is %v\n", &newDog)
 	//*nameP = "Huge Ptr dog"
